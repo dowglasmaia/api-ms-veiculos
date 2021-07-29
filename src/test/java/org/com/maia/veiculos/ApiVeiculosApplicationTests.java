c63ddde6d3870630b3231be018e401f939ae5f12
Dowglas Maia <dowglasmaia@gmail.com>
2021-07-28 22:27:24 -0300

    Start Projeto

---
+++ src/test/java/org/com/maia/veiculos/ApiVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.maia.veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
